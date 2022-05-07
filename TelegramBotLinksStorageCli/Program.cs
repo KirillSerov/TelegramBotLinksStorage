4bf310558bc91ac616f0654d8e5c361cc1d4ed06
KirillSerov <100574249+KirillSerov@users.noreply.github.com>
2022-05-07 13:18:17 +0500

    Создал проект

---
+++ TelegramBotLinksStorageCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramBotLinksStorageCli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
